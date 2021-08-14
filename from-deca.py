8bd20552484499e2a386de57a54454bc282c4585
Guy <mail@geea.us>
2021-08-14 18:33:53 +0300

    fixed conversions

--- from-deca.py
+++ from-deca.py
@@ -4,16 +4,16 @@ if not input_num_str:
 
 convert_to = input("What format whould you like to convert to? hex/oct/bin \n")
 
-if convert == "bin":
+if convert_to == "bin":
     # convert string to num to binary. then remove leading 0b
     print(bin(int(input_num_str))[2:])
 
-if convert == "oct":
+if convert_to == "oct":
     # convert string to num to octa. then remove leading 0o
     print(oct(int(input_num_str))[2:])
 
-if convert == "hex":
+if convert_to == "hex":
     # convert string to num to hexa. then remove leading 0x
     print(hex(int(input_num_str))[2:])
 
-print(max_bin, input_num_str)
+print("Done.")
